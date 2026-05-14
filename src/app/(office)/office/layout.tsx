365d9ad6492706a14929b2b79984628d27e31b76
radatyt2025 <rada.tyt2025@gmail.com>
2026-05-14 16:46:14 +0300

    chore: reinstall dependencies with pnpm

--- src/app/(office)/office/layout.tsx
+++ src/app/(office)/office/layout.tsx
@@ -4,17 +4,14 @@ import styles from '@/css/layout.module.css';
 
 export default function OfficeLayout({
   children,
-  modal,
 }: Readonly<{
   children: React.ReactNode;
-  modal: React.ReactNode;
 }>) {
   return (
     <div className={styles.layout}>
       <Header />
       <main className={styles.main}>
         {children}
-        {modal}
       </main>
       <Footer />
     </div>
